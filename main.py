5d7fc06a09614ba9ce08b63b85c7be3c951a02fc
Daniel Onyeakazi <do4009@hw.ac.uk>
2026-05-12 23:14:15 +0100

    comments

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     (stats, corr), model_text = engine.get_desc(), engine.get_model() 
     print("Descriptive Statistics:\n", stats)
     print("\nCorrelation Matrix:\n", corr)
-    print("\nOLS Regression Summary:\n", model_text)
+    print("\nOLS Regression Summary and Linear Regression Graph:\n", model_text)
     print("\nSpearman Correlations and Elasticities:\n", engine.speartests())
     
     visualizer = Visualizer('DBNomics time series.csv')
